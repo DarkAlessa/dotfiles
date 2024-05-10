061221ef819b5636d19a93f0ebe35a9d7d537215
DarkAlessa <skykick.013@gmail.com>
2024-05-10 09:58:00 +0700

    update prompt

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -1,7 +1,7 @@
 $esc=$([char]27) # esc or `e in ASCII table
-$promtSymbol = " "
+$promtSymbol = "廓"
 
 function Prompt {
     $currentDir = (Get-Location | Get-Item).Name
-    "$esc[38;2;230;230;230m$esc[0m$esc[38;2;0;116;207;48;2;230;230;230m$promtSymbol$esc[0m$esc[38;2;230;230;230m$esc[0m$esc[38;2;0;116;207m$esc[0m$esc[38;2;247;187;7;48;2;0;116;207m  $esc[0m$esc[38;2;255;255;255;48;2;0;116;207m $currentDir $esc[0m$esc[38;2;0;116;207m$esc[0m "
+    "$esc[38;2;230;230;230m$esc[0m$esc[38;2;0;116;207;48;2;230;230;230malice $esc[0m$esc[38;2;255;255;255;48;2;0;116;207m $currentDir $esc[0m$esc[38;2;0;116;207m$esc[0m "
 }
