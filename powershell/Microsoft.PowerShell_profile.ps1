b6332f7b3d9ab9397ecaffb75c49af0f34d8f729
DarkAlessa <skykick.013@gmail.com>
2024-05-10 12:03:37 +0700

    update PS profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,5 @@ $promtSymbol = "廓"
 
 function Prompt {
     $currentDir = (Get-Location | Get-Item).Name
-    "$esc[38;2;230;230;230m$esc[0m$esc[38;2;0;116;207;48;2;230;230;230malice $esc[0m$esc[38;2;255;255;255;48;2;0;116;207m $currentDir $esc[0m$esc[38;2;0;116;207m$esc[0m "
+    "$esc[38;2;230;230;230m$esc[0m$esc[38;2;0;116;207;48;2;230;230;230malice $esc[0m$esc[38;2;255;255;255;48;2;0;116;207m $currentDir$esc[0m$esc[38;2;0;116;207m$esc[0m "
 }
