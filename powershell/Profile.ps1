d935c3e6da428a164258f76603a7a039ce0396ce
DarkAlessa <skykick.013@gmail.com>
2024-05-15 20:27:35 +0700

    ps1 update

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -1,5 +1,5 @@
 $esc=$([char]27) # esc or `e in ASCII table
 function Prompt {
     $currentDir = (Get-Location | Get-Item).Name
-    "$esc[38;2;230;230;230m$esc[0m$esc[38;2;0;116;207;48;2;230;230;230mサクラ$esc[0m$esc[38;2;255;255;255;48;2;0;116;207m $currentDir$esc[0m$esc[38;2;0;116;207m$esc[0m "
+    "$esc[38;2;230;230;230m$esc[0m$esc[38;2;0;116;207;48;2;230;230;230m🌸 サクラ$esc[0m$esc[38;2;230;230;230;48;2;0;116;207m$esc[0m$esc[38;2;255;255;255;48;2;0;116;207m $currentDir$esc[0m$esc[38;2;0;116;207m$esc[0m "
 }
